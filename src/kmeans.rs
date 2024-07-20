10068c8d0940e22341f4dc38861d4b8b5082f7c2
William Keating <54231777+keatingw@users.noreply.github.com>
2024-07-20 23:58:32 +1000

    chore: clean up flow in rust kmeans

--- src/kmeans.rs
+++ src/kmeans.rs
@@ -6,40 +6,54 @@ use rand::seq::index::sample;
 #[pyclass]
 pub struct KMeansRust {
     #[pyo3(get)]
-    pub centers: Vec<Vec<f64>>,
-    #[pyo3(get)]
-    pub allocations: Vec<usize>,
+    pub num_centers: usize,
+
     #[pyo3(get)]
     pub max_iter: usize,
+
+    #[pyo3(get)]
+    pub seed: u64,
+
+    #[pyo3(get)]
+    pub centers: Option<Vec<Vec<f64>>>,
+
+    #[pyo3(get)]
+    pub allocations: Option<Vec<usize>>,
 }
 
 #[pymethods]
 impl KMeansRust {
     /// Create KMeansRust object and fit to the data
     #[new]
-    pub fn new(
-        num_centers: usize,
-        max_iter: usize,
-        data: Vec<Vec<f64>>,
-        seed: u64,
-    ) -> PyResult<Self> {
-        let mut rng = StdRng::seed_from_u64(seed);
+    pub fn new(num_centers: usize, max_iter: usize, seed: Option<u64>) -> PyResult<Self> {
+        Ok(KMeansRust {
+            num_centers,
+            max_iter,
+            seed: seed.unwrap_or_default(),
+            centers: None,
+            allocations: None,
+        })
+    }
+
+    /// Fits kmeans to data given
+    pub fn fit(&mut self, data: Vec<Vec<f64>>) -> PyResult<()> {
+        let mut rng = StdRng::seed_from_u64(self.seed);
 
         // Set initial centers to random set of points
-        let init_center_idx = sample(&mut rng, data.len(), num_centers);
+        let init_center_idx = sample(&mut rng, data.len(), self.num_centers);
         let mut centers: Vec<Vec<f64>> = init_center_idx
             .into_iter()
             .map(|x| data[x].clone())
             .collect();
 
         // start looping to assign points to their clusters
-        let mut allocation: Vec<usize> = vec![0; data.len()];
+        let mut allocations: Vec<usize> = vec![0; data.len()];
         let mut prev_allocation: Vec<usize> = vec![0; data.len()];
 
-        for _ in 0..max_iter {
+        for _ in 0..self.max_iter {
             // For each row, assign to its nearest center based on euclidean distance
             for (rownum, row) in data.iter().enumerate() {
-                allocation[rownum] = centers
+                allocations[rownum] = centers
                     .iter()
                     .map(|c| squared_euclidean_dist(c, row))
                     .enumerate()
@@ -49,9 +63,9 @@ impl KMeansRust {
             }
 
             // For each cluster, recalculate its center after the new allocations
-            for i in 0..num_centers {
+            for i in 0..self.num_centers {
                 // Get all points in the cluster
-                let allocated_to_center = allocation
+                let allocated_to_center = allocations
                     .iter()
                     .zip(&data)
                     .filter_map(|(alloc, x)| if *alloc == i { Some(x.clone()) } else { None })
@@ -69,18 +83,23 @@ impl KMeansRust {
             }
 
             // Basic stopping criterion for if allocations have converged
-            if allocation == prev_allocation {
+            if allocations == prev_allocation {
                 break;
             }
 
             // save prior state and continue iterations
-            prev_allocation = allocation.clone();
+            prev_allocation = allocations.clone();
         }
-        Ok(KMeansRust {
-            centers,
-            allocations: allocation,
-            max_iter,
-        })
+        self.centers = Some(centers);
+        self.allocations = Some(allocations);
+        Ok(())
+    }
+
+    fn __repr__(&self) -> String {
+        format!(
+            "KMeansRust(num_centers={}, max_iter={}, seed={})",
+            self.num_centers, self.max_iter, self.seed,
+        )
     }
 }
 
