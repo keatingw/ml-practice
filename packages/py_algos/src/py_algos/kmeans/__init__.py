2dcef817351f387595ede3a1077527b9cf13cd4a
William Keating <54231777+keatingw@users.noreply.github.com>
2024-09-10 00:23:56 +1000

    refactor: move to workspace setup with subpackages for rust and python

--- packages/py_algos/src/py_algos/kmeans/__init__.py
+++ packages/py_algos/src/py_algos/kmeans/__init__.py
@@ -1,12 +1,9 @@
 """Implementations for KMeans clustering algorithm."""
 
-from ml_practice._lowlevel import KMeansRust
-
 from .native_py import KMeans
 from .numpy_py import KMeansNumpy
 
 __all__ = [
     "KMeans",
     "KMeansNumpy",
-    "KMeansRust",
 ]
